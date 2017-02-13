957006c2498b74d9422e126f15ab451ad16eb125
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-02-12 23:05:24 -0200

    Removed a pycache file Interpreted basic rules of the grammar

--- interpreter.py
+++ interpreter.py
@@ -1,6 +1,32 @@
 '''List intepreter'''
 
+VARIABLE_DEFINITION_ROOT_SCOPE = 'var'
+VARIABLE_DEFINITION_NESTED_SCOPE = 'let'
+VARIABLE_RULE_PARAMETER_OP = 'LEFT_HAND_ASSIGNMENT'
+PRECEDENCE_RULE_PARAMETER_OP = 'HIGH_ORDER_OPERATION'
+DEFAULT_VARIABLE_KIND_RULESET = 'rule'
+DEFAULT_VARIABLE_ASSESMENT = 'root_scope'
+DEFAULT_ASSESMENT_UNSET_VALUE = None
+
 class Grammar(object):
     def __init__(self):
-        self.variable_definition_token = 'let'
-        self.variable_definition_rule = 'LEFT_HAND_ASSIGNMENT'
+        self._variable_definition = {
+            'token': {
+                'root_scope': VARIABLE_DEFINITION_ROOT_SCOPE,
+                'nested_scope': VARIABLE_DEFINITION_NESTED_SCOPE,
+            },
+            'rule': VARIABLE_RULE_PARAMETER_OP,
+        }
+        self._precedence_definition = {
+            'token': ['(',')'],
+            'rule': PRECEDENCE_RULE_PARAMETER_OP,
+        }
+
+    def variable_definition(self,kind=DEFAULT_VARIABLE_KIND_RULESET,assesment=DEFAULT_ASSESMENT_UNSET_VALUE):
+        if kind == 'token':
+            return self._variable_definition[kind][assesment or DEFAULT_VARIABLE_ASSESMENT]
+        else:
+            return self._variable_definition[kind]
+
+    def precedence(self,kind=DEFAULT_VARIABLE_KIND_RULESET):
+        return self._precedence_definition[kind]
--- interpreter_test.py
+++ interpreter_test.py
@@ -5,13 +5,26 @@ import unittest
 from interpreter import Grammar
 
 class GrammarTest(unittest.TestCase):
+    def setUp(self):
+        self.gr = Grammar()
+    
     def test_grammar_instantiation(self):
-        self.assertEqual(Grammar().__class__,Grammar)
+        self.assertEqual(self.gr.__class__,Grammar)
 
-    def test_grammar_variable_definition(self):
-        gr = Grammar()
-        self.assertEqual(gr.variable_definition_token,'let')
-        self.assertEqual(gr.variable_definition_rule,'LEFT_HAND_ASSIGNMENT')
+    def test_grammar_variable_definition_token_root_scope(self):
+        self.assertEqual(self.gr.variable_definition('token','root_scope'),'var')
+
+    def test_grammar_variable_definition_token_nested_scope(self):
+        self.assertEqual(self.gr.variable_definition('token','nested_scope'),'let')
+
+    def test_grammar_variable_definition_rule(self):
+        self.assertEqual(self.gr.variable_definition(),'LEFT_HAND_ASSIGNMENT')
+
+    def test_grammar_precedence_token(self):
+        self.assertEqual(self.gr.precedence('token'),['(',')'])
+
+    def test_grammar_precedence_rule(self):
+        self.assertEqual(self.gr.precedence('rule'),'HIGH_ORDER_OPERATION')
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
